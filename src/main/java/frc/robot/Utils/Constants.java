4261d07fc80159072b7cc0016668e421c56d0fab
331282 <148924183+331282@users.noreply.github.com>
2025-01-14 21:53:06 -0600

    Finnish setting up arm motors

--- src/main/java/frc/robot/Utils/Constants.java
+++ src/main/java/frc/robot/Utils/Constants.java
@@ -96,5 +96,10 @@ public final class Constants {
     public static final Matrix<N3, N1> kSingleTagStdDevs = VecBuilder.fill(4, 4, 8);
     public static final Matrix<N3, N1> kMultiTagStdDevs = VecBuilder.fill(0.5, 0.5, 1);
 
+    public static final double elevatorGearRatio =  1.0; // needs tuning
+    public static final double armGearRadius = 1.0; //needs tuning
+    public static final double coralWheelRadius = 1.0; //needs tunig
+    public static final double algaeWheelRadius = 1.0; //needs tuning
+
 
 }
--- src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -7,12 +7,16 @@ package frc.robot.subsystems;
 import com.revrobotics.spark.SparkClosedLoopController;
 import com.revrobotics.spark.SparkFlex;
 import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkBase;
 import com.revrobotics.spark.SparkBase.ControlType;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.config.SparkFlexConfig;
 import com.revrobotics.spark.config.SparkMaxConfig;
+import com.revrobotics.spark.config.ClosedLoopConfig.FeedbackSensor;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Utils.Constants;
 
 public class ArmSubsystem extends SubsystemBase {
 
@@ -51,7 +55,49 @@ public class ArmSubsystem extends SubsystemBase {
     algaeManipulatorVortexController = algaeManipulatorVortex.getClosedLoopController();
     algaeManipulatorVortexConfig = new SparkFlexConfig();
 
+    elevatorVortexConfig
+      .idleMode(IdleMode.kBrake);
+    elevatorVortexConfig.encoder
+      .positionConversionFactor(Constants.elevatorGearRatio);
+    elevatorVortexConfig.closedLoop
+      .feedbackSensor(FeedbackSensor.kPrimaryEncoder)
+      .pid(1, 0, 0);
 
+    elevatorVortex.configure(elevatorVortexConfig, SparkBase.ResetMode.kResetSafeParameters, SparkBase.PersistMode.kPersistParameters);
+
+
+    rotationMaxConfig
+      .idleMode(IdleMode.kBrake);
+    rotationMaxConfig.encoder
+      .positionConversionFactor(Constants.armGearRadius * Math.PI * 2);
+    rotationMaxConfig.closedLoop
+      .feedbackSensor(FeedbackSensor.kPrimaryEncoder)
+      .pid(1, 0, 0);
+
+    rotationMax.configure(rotationMaxConfig, SparkBase.ResetMode.kResetSafeParameters, SparkBase.PersistMode.kPersistParameters);
+
+
+    coralManipulatorVortexConfig
+      .idleMode(IdleMode.kCoast);
+    coralManipulatorVortexConfig.encoder
+      .velocityConversionFactor(Constants.coralWheelRadius * Math.PI * 2);
+    coralManipulatorVortexConfig.closedLoop
+      .feedbackSensor(FeedbackSensor.kPrimaryEncoder)
+      .pid(1, 0, 0);
+
+    coralManipulatorVortex.configure(coralManipulatorVortexConfig, SparkBase.ResetMode.kResetSafeParameters, SparkBase.PersistMode.kPersistParameters);
+
+
+    algaeManipulatorVortexConfig
+      .idleMode(IdleMode.kCoast);
+    algaeManipulatorVortexConfig.encoder
+      .velocityConversionFactor(Constants.algaeWheelRadius * Math.PI * 2);
+    algaeManipulatorVortexConfig.closedLoop
+      .feedbackSensor(FeedbackSensor.kPrimaryEncoder)
+      .pid(1, 0, 0);
+
+    algaeManipulatorVortex.configure(algaeManipulatorVortexConfig, SparkBase.ResetMode.kResetSafeParameters, SparkBase.PersistMode.kPersistParameters);
+    
   }
 
   @Override
